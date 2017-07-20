25fe27bfe694b3aca507ef4edc2490b9a592a482
FrankArduino <noetzoldt@gmx.de>
2017-07-20 22:36:41 +0200

    Initial commit

---
+++ app/src/main/java/android/frank/test/EWochentag.java
@@ -0,0 +1,15 @@
+package android.frank.test;
+
+/**
+ * Created by Frank on 20.07.2017.
+ */
+
+public enum EWochentag {
+    MONTAG,
+    DIENSTAG,
+    MITTWOCH,
+    DONNERSTAG,
+    FREITAG,
+    SAMSTAG,
+    SONNTAG;
+}
